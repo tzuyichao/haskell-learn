0a829cde247b27f0a58e207943476b4cb25e8d15
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-23 11:03:19 +0800

    guard

--- lesson-2/simple.hs
+++ lesson-2/simple.hs
@@ -4,7 +4,12 @@ simple x = x
 
 calcChange owed given = if change > 0 then change else 0 where change = given - owed
 
+abs2 n | n >= 0    = n
+       | otherwise = -n
+
 main = do
   print(simple("Dog"))
   print(simple(2))
   print(calcChange 50 100)
+  print(abs2(10))
+  print(abs2(-15))
