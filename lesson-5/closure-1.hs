1b38952a6f9fb60519e8f9c00efc7139c0bac0d4
TzuYi Chao <tzuyichao0313@gmail.com>
2021-07-30 11:13:10 +0800

    closure 1

---
+++ lesson-5/closure-1.hs
@@ -0,0 +1,15 @@
+ifEven f x = if even x
+             then f x
+             else x
+
+inc x = x + 1
+
+genIfEven f = (\x -> ifEven f x)
+
+ifEvenInc x = (genIfEven inc) x
+
+main = do
+    let x = 10
+    print x
+    let x1 = ifEvenInc x
+    print x1
